fb3e52c6ebac0da7f43684e7c646a4af0bfd260e
Administrator <Pooya>
2018-03-12 21:13:15 +0000

    Read data from DB to a C# object.

--- myCoreMvc/Controllers/ExperimentsController.cs
+++ myCoreMvc/Controllers/ExperimentsController.cs
@@ -4,47 +4,18 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using myCoreMvc.Services;
 using myCoreMvc.Models;
 
 namespace myCoreMvc.Controllers
 {
     public class ExperimentsController : Controller
     {
-        [Route("Db")]
-        public ActionResult Db()
+        [Route("db")]
+        public ActionResult Database()
         {
-            var myDBconnection = new SqlConnection("Database=myCoreMvc; SERVER=.\\sqlexpress; Integrated Security=SSPI; MultipleActiveResultSets=True;");
-            myDBconnection.Open();
-            var myParam = new SqlParameter("@Param1", System.Data.SqlDbType.Int);
-            myParam.Value = "5";
-            var selectCommand = new SqlCommand("select * from [myCoreMvc].[dbo].[WorkItems] where Id = @Param1", myDBconnection);
-            selectCommand.Parameters.Add(myParam);
-            var myReader = selectCommand.ExecuteReader();
-            var properties = typeof(WorkItem).GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
-            var instance = new WorkItem();
-            if (myReader.Read())
-            {
-                foreach (var prop in properties)
-                {
-                    //result += " " + prop.Name + ": " + myReader[prop.Name].ToString();
-                    typeof(WorkItem).GetProperty(prop.Name).SetValue(instance, myReader[prop.Name]);
-                }
-            }
-            myDBconnection.Close();
+            var instance = Db.Get<WorkItem>(5);
             return View("~/Views/ListOfWorkItems/ListOfWorkItemsDetails.cshtml", instance);
-            //return Content(result);
-
-            //    [Route("Add")]
-            //public string Add()
-            //{
-            //    var myDBconnection = new SqlConnection("Database=Prime247.Temp; SERVER=.\\sqlexpress; Integrated Security=SSPI; MultipleActiveResultSets=True;");
-            //    var insertCommand = new SqlCommand("insert into [Prime247.Temp].[dbo].[Banks] (Id, Name, AccountNumber, SortCode, AccountName) values ('d83b1b6e-5e14-476e-9f05-cff01d35a7e9', 'DasooName', '321', '123', 'DasooAccountName')", myDBconnection);
-            //    myDBconnection.Open();
-            //    insertCommand.ExecuteNonQuery();
-            //    myDBconnection.Close();
-            //    return "Added";
-            //}
-
         }
 
         [Route("delegate")]
---
+++ myCoreMvc/Services/Db.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using myCoreMvc.Models;
+using System.Data.SqlClient;
+
+namespace myCoreMvc.Services
+{
+    public class Db
+    {
+        public static T Get<T>(int id)
+        {
+            var myDBconnection = new SqlConnection("Database=myCoreMvc; SERVER=.\\sqlexpress; Integrated Security=SSPI; MultipleActiveResultSets=True;");
+            myDBconnection.Open();
+            var myParam = new SqlParameter("@Param1", id);
+            var selectCommand = new SqlCommand("select * from [myCoreMvc].[dbo].[WorkItems] where Id = @Param1", myDBconnection);
+            selectCommand.Parameters.Add(myParam);
+            var myReader = selectCommand.ExecuteReader();
+            var properties = typeof(T).GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
+            var instance = (T)Activator.CreateInstance(typeof(T));
+            if (myReader.Read())
+            {
+                foreach (var property in properties)
+                {
+                    typeof(T).GetProperty(property.Name).SetValue(instance, myReader[property.Name]);
+                }
+            }
+            myDBconnection.Close();
+            return instance;
+        }
+
+        //public string Add()
+        //{
+        //    var myDBconnection = new SqlConnection("Database=Prime247.Temp; SERVER=.\\sqlexpress; Integrated Security=SSPI; MultipleActiveResultSets=True;");
+        //    var insertCommand = new SqlCommand("insert into [Prime247.Temp].[dbo].[Banks] (Id, Name, AccountNumber, SortCode, AccountName) values ('d83b1b6e-5e14-476e-9f05-cff01d35a7e9', 'DasooName', '321', '123', 'DasooAccountName')", myDBconnection);
+        //    myDBconnection.Open();
+        //    insertCommand.ExecuteNonQuery();
+        //    myDBconnection.Close();
+        //    return "Added";
+        //}
+    }
+}
