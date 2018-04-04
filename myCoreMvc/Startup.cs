3e76838a8dbb6feab2634874db53841abb51edd3
Administrator <Pooya>
2018-04-04 23:00:21 +0100

    Enabled static file serving + their path mapping.

--- myCoreMvc/Startup.cs
+++ myCoreMvc/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace myCoreMvc
@@ -28,6 +30,12 @@ namespace myCoreMvc
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var staticFileOptions = new StaticFileOptions();
+            staticFileOptions.RequestPath = "/StaticContent";
+            var path = Path.Combine(env.ContentRootPath, "StaticFiles");
+            staticFileOptions.FileProvider = new PhysicalFileProvider(path);
+            app.UseStaticFiles(staticFileOptions);
 
             app.UseMvc(routes =>
             {
