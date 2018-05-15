29ec788ad96fb373ce7faa5d4fa6316ac9b926b7
Administrator <Pooya>
2018-05-15 23:42:11 +0100

    Implemented delete function for WorkPlans.

--- myCoreMvc/Controllers/ListOfWorkPlans/DetailsOfWorkPlan.cs
+++ myCoreMvc/Controllers/ListOfWorkPlans/DetailsOfWorkPlan.cs
@@ -14,5 +14,17 @@ namespace myCoreMvc
             var viewModel = DataProvider.Get<WorkPlan>(wi => wi.Id == id);
             return View("~/Views/ListOfWorkPlans/DetailsOfWorkPlan.cshtml", viewModel);
         }
+
+        public IActionResult Delete(Guid id)
+        {
+            var result = "";
+            switch (DataProvider.Delete<WorkPlan>(id))
+            {
+                case DataProvider.TransactionResult.NotFound: result = "Found no WorkPlan with the provided Id."; break;
+                case DataProvider.TransactionResult.Deleted: result = "Item deleted."; break;
+                default: result = "Found no WorkPlan with the provided Id."; break;
+            }
+            return RedirectToAction("Index", "ListOfWorkPlans", new { message = result });  // Prevents form re-submission by refresh
+        }
     }
 }
