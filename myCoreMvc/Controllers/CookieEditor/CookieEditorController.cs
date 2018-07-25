0199fcccbb1f4254ff62f7b7fd5c986b8386846a
Pooya <pooya.bazzaz@geeks.ltd.uk>
2018-07-25 17:25:01 +0100

    Added CookieEditor

---
+++ myCoreMvc/Controllers/CookieEditor/CookieEditorController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using myCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PooyasFramework;
+using PooyasFramework.Attributes;
+using myCoreMvc.Services;
+
+namespace myCoreMvc.Controllers
+{
+    public class CookieEditorController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var inputModel = new EnterModel();
+            inputModel.Cookies = Request.Cookies;
+            return View("~/Views/CookieEditor/EnterWorkItem.cshtml", inputModel);  // TODO: Use "asp-" tag helpers instead of tags attributes.
+        }
+
+        [HttpPost]
+        [CustomExceptionFilter]
+        public IActionResult Index(EnterModel inputModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var workItem = new WorkItem();
+                // TODO: Look into object mapping solutions like AutoMapper to learn best practices.
+                // ModelState.AddModelError("Reference", "It must be in blabla format!")
+                // ModelState.AddModelError("", "This is an object level error rather than property level.")
+                // @Html.ValidationSummary(true)
+                // @Html.ValidationMessageFor(p => p.Reference)
+                workItem.CopySimilarPropertiesFrom(inputModel);  // We use this simple way to prevent malicious over-posting
+                workItem.WorkPlan = DataProvider.Get<WorkPlan>(inputModel.WorkPlan);
+                TransactionResult transactionResult;
+                if (workItem.Id == Guid.Empty)
+                {
+                    transactionResult = DataProvider.Add(workItem);
+                }
+                else
+                {
+                    transactionResult = DataProvider.Update(workItem);
+                }
+                var resultMessage = "";
+                switch (transactionResult)
+                {
+                    case TransactionResult.Updated: resultMessage = "Item updated"; break;
+                    case TransactionResult.Added: resultMessage = "New item added"; break;
+                    default: resultMessage = transactionResult.ToString(); break;
+                }
+                return RedirectToAction(nameof(ListOfWorkItemsController.Index), ShortNameOf<ListOfWorkItemsController>(), new { message = resultMessage });  // Prevents re-submission by refresh
+            }
+            else
+            {
+                inputModel.Message = "Invalid values for: "
+                    + ModelState.Where(p => p.Value.ValidationState == ModelValidationState.Invalid).Select(p => p.Key).ToString(", ");
+                return View("~/Views/ListOfWorkItems/EnterWorkItem.cshtml", inputModel);
+            }
+        }
+
+        public class EnterModel : IClonable
+        {
+            public Dictionary Cookies { get; set; }
+        }
+    }
+}
