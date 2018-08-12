edea80f933e0081d47dd38b095a1a81c6db67fe4
Administrator <Pooya>
2018-08-12 18:25:41 +0100

    Updated myTasks.

--- myCoreMvc/myTasks.cs
+++ myCoreMvc/myTasks.cs
@@ -1,4 +1,8 @@
-﻿//Task: Use Selenium to automate web GUI testing. Look into Katalon Studio as a full feature IDE.
+﻿//FuturePlan: Further develop upon authorisation and authentication
+//Experiment: Google Authentication using OIDC based on:
+//https://app.pluralsight.com/player?course=aspdotnet-core-fundamentals&author=scott-allen&name=aspdotnet-core-fundamentals-m7&clip=5&mode=live
+
+//Task: Use Selenium to automate web GUI testing. Look into Katalon Studio as a full feature IDE.
 
 //FuturePlan: OnBound and similar methods are in fact attributes declared in "Msharp.Framework.Mvc.Version\src\Attributes"
 // See if you can use OnActionExecuting() and OnActionExecuted() or similar methods from these pages to achieve similar functionality:
