5b785dfcf01976e2c4ee75203fbf22244068351e
Pooya <pooya.bazzaz@geeks.ltd.uk>
2018-08-31 17:57:21 +0100

    Further developed my Author Tag to replace complex markup and script in the footer.

---
+++ myCoreMvc/A_PooyasFramework/TagHelpers/ClientSocketTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace myCoreMvc.PooyasFramework
+{
+    public class ClientSocketTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            var content = @"<div>Your network socket: <span id='client-ip'></span></div>
+                            <script>window.onload = function() {
+                             var url = window.location.origin + '/GetClientSocket';
+                             var ajaxRequest = new XMLHttpRequest();
+                             ajaxRequest.onreadystatechange = function() {
+                              if (this.readyState === 4 && this.status === 200)
+                              {
+                               document.getElementById('client-ip').innerHTML = this.responseText;
+                              }
+                             };
+                             ajaxRequest.open('GET', url, true);
+                             ajaxRequest.send();
+                             };
+                            </script>";
+
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
