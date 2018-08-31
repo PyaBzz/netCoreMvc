704c26c059acf7cecb10e26fc8a327108be1d70a
Administrator <Pooya>
2018-08-31 01:28:35 +0100

    Simplified Razor views by extending my HTML helper

--- myCoreMvc/A_PooyasFramework/Extensions.cs
+++ myCoreMvc/A_PooyasFramework/Extensions.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace PooyasFramework
 {
@@ -111,6 +113,21 @@ namespace PooyasFramework
         public static string ShortNameOf<T>(this IHtmlHelper helper) where T : Controller
         {
             return typeof(T).Name.TrimEnd("Controller");
+        }
+
+        public static IHtmlContent LinkToAction<T>(this IHtmlHelper thisHelper, string actionName, string linkText) where T : Controller
+        {
+            return thisHelper.ActionLink(linkText, actionName, thisHelper.ShortNameOf<T>());
+        }
+
+        public static IHtmlContent LinkToAction<T>(this IHtmlHelper thisHelper, string actionName, string linkText, Guid routeId) where T : Controller
+        {
+            return thisHelper.ActionLink(
+                linkText: linkText,
+                actionName: actionName,
+                controllerName: thisHelper.ShortNameOf<T>(),
+                routeValues: new { id = routeId }
+                );
         }
     }
 }
