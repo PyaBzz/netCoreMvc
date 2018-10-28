1d382488a7af04221279b241efbdf194a0fff05c
Administrator <Pooya>
2018-10-28 12:10:06 +0000

    Added DbCtx with hardcoded connection string.

---
+++ myCoreMvc/B_Services/Data/DbCtx.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myCoreMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myCoreMvc.Services
+{
+    public class DbCtx : DbContext
+    {
+        public DbCtx(DbContextOptions options) : base(options) { }
+
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<WorkPlan> WorkPlans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("SERVER=.\\sqlexpress; Database=myCoreMvc; MultipleActiveResultSets=True; Integrated Security=SSPI;");
+        }
+    }
+}
