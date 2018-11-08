1fb465f4c87acb368515b7daf0ee5f0a22011142
Pooya <pooya.bazzaz@geeks.ltd.uk>
2018-11-08 17:13:45 +0000

    Used Ctx.Set<T>() to get datasets without need for GetDbSet<T>() method.

--- myCoreMvc/B_Services/Data/EfDataProvider.cs
+++ myCoreMvc/B_Services/Data/EfDataProvider.cs
@@ -21,39 +21,32 @@ namespace myCoreMvc.Services
             Ctx = ctx;
         }
 
-        private DbSet<T> GetDbSet<T>() where T : Thing
-        {
-            var targetPropertyInfo = Ctx.GetType().GetPublicDeclaredInstancePropertyInfos()
-                .Single(pi => pi.PropertyType == typeof(DbSet<T>));
-            return targetPropertyInfo.GetValue(Ctx) as DbSet<T>;
-        }
-
         /*==================================  Methods of IDataProvider =================================*/
 
         public TransactionResult Add<T>(T obj) where T : Thing
         {
-            GetDbSet<T>().Add(obj); //Task: We could simply say Ctx.Add(obj) and let it figure type and stuff
+            Ctx.Set<T>().Add(obj); //Task: We could simply say Ctx.Add(obj) and let it figure type and stuff
             return Ctx.SaveChanges() == 0 ? TransactionResult.Failed : TransactionResult.Added;
         }
 
-        public T Get<T>(Guid id) where T : Thing => GetDbSet<T>().Find(id); //Find() is more performant than GetList etc.
+        public T Get<T>(Guid id) where T : Thing => Ctx.Set<T>().Find(id); //Find() is more performant than GetList etc.
 
-        public T Get<T>(Func<T, bool> func) where T : Thing => GetDbSet<T>().SingleOrDefault(t => func(t)); //Single() is a Linq to Entity execution method
+        public T Get<T>(Func<T, bool> func) where T : Thing => Ctx.Set<T>().SingleOrDefault(t => func(t)); //Single() is a Linq to Entity execution method
 
-        public List<T> GetList<T>() where T : Thing => GetDbSet<T>().ToList(); //ToList() is a Linq to Entity execution method
+        public List<T> GetList<T>() where T : Thing => Ctx.Set<T>().ToList(); //ToList() is a Linq to Entity execution method
 
-        public List<T> GetList<T>(Func<T, bool> func) where T : Thing => GetDbSet<T>().Where(t => func(t)).ToList();
+        public List<T> GetList<T>(Func<T, bool> func) where T : Thing => Ctx.Set<T>().Where(t => func(t)).ToList();
 
         public TransactionResult Update<T>(T obj) where T : Thing
         {
-            GetDbSet<T>().Update(obj);
+            Ctx.Set<T>().Update(obj);
             return Ctx.SaveChanges() == 0 ? TransactionResult.NotFound : TransactionResult.Updated;
         }
 
         public TransactionResult Delete<T>(Guid id) where T : Thing
         {
             var target = Get<T>(id); // To delete using a single DB trip, we can use Ctx.Database.ExecuteSqlCommand("exec DeleteById {0}", id) or DbSet.FromSql()
-            GetDbSet<T>().Remove(target);
+            Ctx.Set<T>().Remove(target);
             return Ctx.SaveChanges() == 0 ? TransactionResult.NotFound : TransactionResult.Deleted;
         }
     }
