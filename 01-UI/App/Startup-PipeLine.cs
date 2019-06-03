938f11eb7a8fe900fc222b6a599c7458946676e8
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-06-03 17:46:15 +0100

    Organise Startup and Program class in folders

---
+++ 01-UI/App/Startup-PipeLine.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+using PyaFramework.Middleware;
+
+namespace myCoreMvc.UI
+{
+    public partial class Startup
+    {
+        //##################################################################
+        //###                          Pipeline                          ###
+        //##################################################################
+
+        public void Configure(IHostingEnvironment env, IApplicationBuilder appBuilder)
+        {
+            //Lesson:
+            #region
+            // The "IsDevelopment" method returns true only if EnvironmentName equals EnvironmentName.Development (string)
+            // Our environment is currently called "DevProj"
+            //if (env.IsDevelopment()) appBuilder.UseDeveloperExceptionPage();
+            #endregion
+            appBuilder.UseDeveloperExceptionPage();
+
+            appBuilder.UseMiddleware<CustomMiddleware>();
+
+            //TODO: Redirect to SSH using this:
+            //appBuilder.UseRewriter(new Microsoft.AspNetCore.Rewrite.RewriteOptions().AddRedirectToHttpsPermanent());
+
+            var staticFileOptions = new StaticFileOptions();
+            staticFileOptions.RequestPath = "/StaticContent";
+            var path = Path.Combine(env.ContentRootPath, "StaticFiles");
+            staticFileOptions.FileProvider = new PhysicalFileProvider(path);
+            appBuilder.UseStaticFiles(staticFileOptions);
+
+            appBuilder.UseAuthentication();
+
+            //Experience: When this is put before Authentication middleware it doesn't work. Why?
+            appBuilder.UseMiddleware<AntiForgeryTokenValidatorMiddleware>();
+
+            appBuilder.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{area=WorkItems}/{controller=WorkItemList}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
--- 01-UI/App/Startup-Services.cs
+++ 01-UI/App/Startup-Services.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
-using PyaFramework.Middleware;
 using PyaFramework.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Authorization;
@@ -17,7 +13,7 @@ using myCoreMvc.UI.Controllers;
 
 namespace myCoreMvc.UI
 {
-    public class Startup
+    public partial class Startup
     {
         //##################################################################
         //###                          Services                          ###
@@ -109,42 +105,6 @@ namespace myCoreMvc.UI
                 o.AreaViewLocationFormats.Add("/Pages/{2}/{1}/{0}" + RazorViewEngine.ViewExtension);
                 o.AreaViewLocationFormats.Add("/Pages/{2}/{0}" + RazorViewEngine.ViewExtension);
                 o.AreaViewLocationFormats.Add("/Pages/zzShared/{0}" + RazorViewEngine.ViewExtension);
-            });
-        }
-
-        //##################################################################
-        //###                          Pipeline                          ###
-        //##################################################################
-
-        public void Configure(IHostingEnvironment env, IApplicationBuilder appBuilder)
-        {
-            //Lesson:
-            #region
-            // The "IsDevelopment" method returns true only if EnvironmentName equals EnvironmentName.Development (string)
-            // Our environment is currently called "DevProj"
-            //if (env.IsDevelopment()) appBuilder.UseDeveloperExceptionPage();
-            #endregion
-            appBuilder.UseDeveloperExceptionPage();
-
-            appBuilder.UseMiddleware<CustomMiddleware>();
-
-            //TODO: Redirect to SSH using this:
-            //appBuilder.UseRewriter(new Microsoft.AspNetCore.Rewrite.RewriteOptions().AddRedirectToHttpsPermanent());
-
-            var staticFileOptions = new StaticFileOptions();
-            staticFileOptions.RequestPath = "/StaticContent";
-            var path = Path.Combine(env.ContentRootPath, "StaticFiles");
-            staticFileOptions.FileProvider = new PhysicalFileProvider(path);
-            appBuilder.UseStaticFiles(staticFileOptions);
-
-            appBuilder.UseAuthentication();
-
-            //Experience: When this is put before Authentication middleware it doesn't work. Why?
-            appBuilder.UseMiddleware<AntiForgeryTokenValidatorMiddleware>();
-
-            appBuilder.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{area=WorkItems}/{controller=WorkItemList}/{action=Index}/{id?}");
             });
         }
     }
