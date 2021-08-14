8034c70d12fbd905643ed9ab9ef8c7e505e2dfec
pyabzz <>
2021-08-14 10:21:48 +0100

    Tidy up

--- 21-App.test/DataLayer/UserRepoTest.cs
+++ 21-App.test/DataLayer/UserRepoTest.cs
@@ -19,9 +19,9 @@ namespace myCoreMvc.Test.DataLayer
             this.repo = rep;
             var salts = hashFac.GetSalt(3);
 
-            jim = new User { Salt = salts[0], Name = "Jim", Hash = hashFac.GetHash("jjj", salts[0]), DateOfBirth = new DateTime(2018, 01, 22), Role = AuthConstants.JuniorRoleName };
-            sam = new User { Salt = salts[1], Name = "Sam", Hash = hashFac.GetHash("sss", salts[1]), DateOfBirth = new DateTime(2010, 01, 22), Role = AuthConstants.SeniorRoleName };
-            adam = new User { Salt = salts[2], Name = "Adam", Hash = hashFac.GetHash("aaa", salts[2]), DateOfBirth = new DateTime(2000, 01, 22), Role = AuthConstants.AdminRoleName };
+            jim = new User { Name = "Jim", Role = AuthConstants.JuniorRoleName, DateOfBirth = new DateTime(2018, 01, 22), Salt = salts[0], Hash = hashFac.GetHash("jjj", salts[0]) };
+            sam = new User { Name = "Sam", Role = AuthConstants.SeniorRoleName, DateOfBirth = new DateTime(2010, 01, 22), Salt = salts[1], Hash = hashFac.GetHash("sss", salts[1]) };
+            adam = new User { Name = "Adam", Role = AuthConstants.AdminRoleName, DateOfBirth = new DateTime(2000, 01, 22), Salt = salts[2], Hash = hashFac.GetHash("aaa", salts[2]) };
         }
 
         public void Dispose()
